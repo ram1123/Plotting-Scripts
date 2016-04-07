46ceaceee46d2c1bad185ecf78faaa68470b7633
ram1123 <ramkrishna.sharma71@gmail.com>
2016-04-07 19:53:02 +0530

    Updated Code: Now this will put data from different text file into one root file and its plot with gaussian fit

--- HistoPlot.C
+++ HistoPlot.C
@@ -15,8 +15,9 @@
  *
  * =====================================================================================
  */
-#include <vector>
+
 #include <iostream>
+#include <vector>
 #include <string>
 #include "TROOT.h"
 #include "TCanvas.h"
@@ -31,67 +32,127 @@
 #include "TH1.h"
 #include "TF1.h"
 
-void HistoPlot(string inputfile, string OutputFileName, 	// Input & output files
-	int nbins, double hmin, double hmax,			// Histo discription
-	double constant, double mean, double sigma, 		// Gauss input parameters
-	bool FixPar, double minA, double maxA, double minM, double maxM, // Restrict gauss parameters
-	bool getStat						// Show stat or not
-	)
+void HistoPlot(	/* bool FixPar, bool getStat	*/					// Show stat or not
+		)
 {
-  if (getStat)
+  TFile *file = new TFile("Timing_Data_07042016.root","RECREATE");
+  //TDirectory *cditof = file->mkdir("tof");
+
+
+  //if (getStat)
+  if (1)
   {
-  gStyle->SetOptStat(1111);
+  //gStyle->SetOptStat(1111);
   gStyle->SetOptFit(kTRUE);
   }
   else
   gStyle->SetOptStat(0);
 
+  string InputFileName, TreeName, OutPutFileName;
+  int Nbins, min, max;
+  vector<string> v_InputFileName, v_TreeName, v_OutPutFileName;
+  vector<int> v_Nbins, v_min, v_max;
 
-  TCanvas *c1= new TCanvas("c1"," ");
-  ifstream in (inputfile.c_str(),std::ios_base::in);
-  vector<double> data;
-
-
- TH1F *h1 = new TH1F("h1","",nbins, hmin, hmax);
- cout<<"nbins = "<<nbins<<"\thmin = "<<hmin<<"\thmax = "<<hmax<<endl;
- h1->GetXaxis()->SetTitle("Time Resolution (ns)");
- h1->GetYaxis()->SetTitle("Counts");
- h1->GetXaxis()->CenterTitle();
- h1->GetYaxis()->CenterTitle();
- h1->GetYaxis()->SetTitleOffset(1.5);
- h1->SetLineColor(1);
- h1->SetLineWidth(2);
-
-float line;
-while (in >> line)
+  ifstream infile;
+  infile.open ("Template.txt");
+  string line; 
+  int count_Dir = 0;
+  string Temp_Dir;
+  vector<string> Dir_Names;
+  int i = 0;
+  
+  while(getline(infile,line))
 	{
-	data.push_back(line);
-	h1->Fill(line);
+	line.erase(line.begin(), find_if(line.begin(), line.end(), not1(ptr_fun<int, int>(isspace)))); 
+
+        if(line[0] == '#') continue;
+
+	stringstream(line) >> InputFileName >> TreeName >> OutPutFileName >> Nbins >> min >> max;
+
+	v_InputFileName.push_back(InputFileName);
+	v_TreeName.push_back(TreeName);
+	v_OutPutFileName.push_back(OutPutFileName);
+	v_Nbins.push_back(Nbins);
+	v_min.push_back(min);
+	v_max.push_back(max);
+        
+	if (i==0)	
+	{
+	Temp_Dir = TreeName;
+	Dir_Names.push_back(TreeName);
 	}
+	if (i>0)
+	{
+		if (Temp_Dir != TreeName)
+			{
+			count_Dir++;
+			Temp_Dir = TreeName;
+			Dir_Names.push_back(TreeName);
+			}
+	}
+	//cout<<"Temp_Dir = "<<Temp_Dir<<endl;
+	//cout<<"InputFileName = "<< InputFileName <<"\tTreeName = "<<TreeName<<"\tOutPutFileName = "<<OutPutFileName<<"\tNbins = "<<Nbins<<"\t min = "<<min<<"\tmax = "<<max<<endl;
+	i++;
+	} 
+  infile.close();
 
-h1->Scale(1/h1->Integral());
-TF1 *f1 = new TF1("f1","[0]*exp(-((x-[1])*(x-[1]))/((2*([2]*[2]+1.27*1.27))))",hmin, hmax); 
-f1->SetParameters(constant, mean, sigma);
-if (FixPar)
-{
-	//f1->SetParLimits(0, minA, maxA);
-	//f1->SetParLimits(1, minM, maxM);
-	f1->SetParLimits(0, (double)h1->GetBinContent(h1->GetMaximumBin()), h1->GetBinContent(h1->GetMaximumBin())+1.0);
-	f1->SetParLimits(1, h1->GetMean()-1.0, h1->GetMean()+1.0);
-}
-f1->SetParNames("Constant","Mean","Sigma");  
- f1->SetLineWidth(2);
- f1->SetLineColor(6);
+  TDirectory **TD = new TDirectory*[Dir_Names.size()];
+  TNtuple **ntuple = new TNtuple*[v_InputFileName.size()];
+  TH1F **th = new TH1F*[v_InputFileName.size()];
+//  TCanvas *c1 = new TCanvas("test");
+//  TCanvas ** tc = new TCanvas*[v_InputFileName.size()];
 
-  h1->Draw();
-h1->Fit(f1,"R");
-TLatex *txt1 = new TLatex(1,100,"Text to show");
-txt1->SetTextSize(0.03);
-txt1->Draw("same");
-string OutputFileNamePDF = OutputFileName+".pdf";
-string OutputFileNamePNG = OutputFileName+".png";
-string OutputFileNameC = OutputFileName+".C";
-c1->SaveAs(OutputFileNamePDF.c_str());
-c1->SaveAs(OutputFileNamePNG.c_str());
-//c1->SaveAs(OutputFileNameC.c_str());
+  for (int dirn = 0; dirn<Dir_Names.size(); dirn++)
+		TD[dirn] = file->mkdir(Dir_Names[dirn].c_str());
+//	cout<<"=============================== Dir Name = "<<Dir_Names[dirn]<<endl;
+
+  
+  int countDir = 0;
+  string treeName;
+  for (int fileNum = 0; fileNum<v_InputFileName.size(); fileNum++)
+  {
+//	c1->cd();
+	//cout<<"v_InputFileName : "<<v_InputFileName[fileNum]<<endl;
+	ntuple[fileNum] = new TNtuple(v_OutPutFileName[fileNum].c_str(),v_OutPutFileName[fileNum].c_str(),v_OutPutFileName[fileNum].c_str());
+	th[fileNum] = new TH1F(Form("th%i",fileNum),v_OutPutFileName[fileNum].c_str(),25,0,40);
+
+	if (fileNum == 0) 
+	{
+		treeName = v_TreeName[fileNum];		TD[fileNum]->cd();
+	}
+	if ( treeName != v_TreeName[fileNum] ) {
+		countDir++;		TD[countDir]->cd();		treeName = v_TreeName[fileNum];
+		cout<<"Tree Name = "<<treeName<<endl;
+	}
+	
+		cout<<"Outside loop Tree Name = "<<treeName<<endl;
+		
+	ifstream in (v_InputFileName[fileNum].c_str(),std::ios_base::in);	// Read new data file
+	while (in >> line)							// loop over all the entries in the loaded data file
+	{
+	double temp = ::atof(line.c_str());
+	ntuple[fileNum]->Fill(temp);	th[fileNum]->Fill(temp);
+	}						// while loop ends
+	in.close();					// close the loded data file
+//	ntuple[fileNum]->Write();			// write the ntuple in the root file
+
+	TF1 *f1 = new TF1("f1","[0]*exp(-((x-[1])*(x-[1]))/((2*([2]*[2]+1.27*1.27))))",10,30); 
+	f1->SetParameters((double)th[fileNum]->GetBinContent(th[fileNum]->GetMaximumBin()), th[fileNum]->GetMean(), 1.0);
+	bool FixPar = 1;
+	if (FixPar)
+	{
+		f1->SetParLimits(0, (double)th[fileNum]->GetBinContent(th[fileNum]->GetMaximumBin()), th[fileNum]->GetBinContent(th[fileNum]->GetMaximumBin())+2.0);
+		f1->SetParLimits(1, th[fileNum]->GetMean()-2.0, th[fileNum]->GetMean()+2.0);
+	}
+	f1->SetParNames("Constant","Mean","Sigma");  
+	f1->SetLineWidth(2);
+ 	f1->SetLineColor(6);
+
+	th[fileNum]->Draw();
+	th[fileNum]->Fit(f1,"RQ");
+//	c1->Write();
+//	c1->Clear();
+  }
+  file->Write();
+  delete file;
 }
