4d580b7f40248e1bec0be5a5bd926a9312340874
Ram Krishna Sharma <ramkrishna.sharma71@gmail.com>
2016-08-23 18:54:45 +0200

    updated print method in .C file

--- Template.py
+++ Template.py
@@ -3,9 +3,9 @@ InputData="SpaceResolutionInfo.txt"
 xlabel="Current Supplied To HV Divider (#mu A)"
 ylabel="Space Resolution (#mu rad)"
 
-VarInTextFile=["CurrentSuppliedToHVDivider","Space_Resolution","Err_SpaceResolution"]
+VarInTextFile=["CurrentSuppliedToHVDivider","Space_Resolution","Err_SpaceResolution"]  # Each element of list corresponds to one column of intput text file
 
-GraphTitle="Space Resolution Vs Current Supplied to HV Divider"
+GraphTitle="Space Resolution Vs Current Supplied to HV Divider"	
 
 legends=[
 	"Threshold = 1.2 fC"
@@ -16,17 +16,18 @@ legends=[
 	,"(i#eta,i#phi)=(5,2)"
 	]
 
-pos1=770	
-pos2=1280
-sep=1.5
-cpos1=734
-cpos2=1385
-cpos3=798
+pos1=770		# Legend x pos
+pos2=1280		# Legend y pos
+sep=1.5			# y-distance b/w two legends fraction
+cpos1=734		# CMS Prem x pos
+cpos2=1385		# CMS Prem y pos
+cpos3=798		# DetInfo position
+DetInfo="GE11_IV"	# Detector Info
 
-yrange=[1140,1380]
+yrange=[1140,1380]	# Y range
 
-xoffset=1.2
-yoffset=1.9
+xoffset=1.2		# X-offset
+yoffset=1.9		# Y-offset
 
-setMaxdigit=4
-getstat=0
+setMaxdigit=4		# SetMaximumDigit
+getstat=0		# GetStats
--- makeCppGraphMaker.py
+++ makeCppGraphMaker.py
@@ -16,103 +16,114 @@ print "// ############################################################"
 print "// # Usage                                                    #"
 print "// #                                                          #"
 print "// ############################################################"
-
+print ''
 print '#include "TGraphErrors.h"'
 print '#include "setTDRStyle.C"'
+print ''
+print ''
 
 print 'void %s(){'%OutPutCodeName
-print 'ifstream in;'
-print 'in.open("%s");'%InputData
-print 'TGaxis::SetMaxDigits(%i);'%setMaxdigit
-print 'gStyle->SetOptStat(%i);'%getstat
-print '// Set TDR Style'
-print 'setTDRStyle();'
-
-print 'Float_t var1, var2, var3;'
-print 'vector<float> v_var1, v_var2, v_var3;'
-
-print 'Int_t nlines = 0;'
-print 'TFile *f = new TFile("%s.root","RECREATE");'%OutPutCodeName
-print 'TNtuple *ntuple = new TNtuple("ntuple","data from ascii file","%s:%s:%s");'%(VarInTextFile[0],VarInTextFile[1],VarInTextFile[2])
-print 'TCanvas* c1 = new TCanvas("c1","",1);'
-print 'c1->Range(0,0,1,1);'
-print 'TPad *pad = new TPad("pad","",0,0,1,1);'
-print 'pad->SetGrid();'
-print 'pad->Draw();'
-print 'pad->cd();'
-
-print 'string line;'
-print 'while(getline(in,line))'
-print '{'
-print "        if(line[0] == '#') continue;"
+print '\tifstream in;'
+print '\tin.open("%s");'%InputData
 print ''
-print '        stringstream(line) >> var1 >> var2 >> var3;'
-print '        '
-print '        cout<<"===> "<<var1 <<"\t "<<var2<<"\t"<<var3<<endl;'
-print '        v_var1.push_back(var1);'
-print '        v_var2.push_back(var2);'
-print '        v_var3.push_back(var3);'
-print '        ntuple->Fill(var1,var2,var3);'
-print '}'
-
-print '   in.close();'
+print '\tTGaxis::SetMaxDigits(%i);'%setMaxdigit
+print '\tgStyle->SetOptStat(%i);'%getstat
 print ''
-print '   TGraphErrors * gr = new TGraphErrors(v_var1.size()); '
-print '    '
-print '   for (unsigned int i = 0; i<v_var1.size();i++)'
-print '{'
-print '        gr->SetPoint(i,v_var1[i],v_var2[i]);'
-print '        gr->SetPointError(i,0,v_var3[i]);'
-print '}'
-
-
-
-print '   gr->SetTitle("");'
-print '   //gr->SetTitle("var1 vs var2");'
-print '   gr->GetXaxis()->SetTitle("%s");'%xlabel
-print '   gr->GetYaxis()->SetTitle("%s");'%ylabel
-print '   gr->GetYaxis()->SetTitleOffset(%i);'%yoffset
-print '   gr->GetXaxis()->SetTitleOffset(%i);'%xoffset
-print '   gr->GetYaxis()->SetTitleSize(0.05);'
-print '   gr->GetXaxis()->SetTitleSize(0.05);'
-print '   //gr->GetXaxis()->SetLabelSize(0.05);'
-print '   gr->GetYaxis()->SetRangeUser(%i,%i);'%(yrange[0],yrange[1])
-print '   gr->SetMarkerSize(1);'
-print '   gr->SetMarkerColor(2);'
-print '   gr->SetMarkerStyle(21);'
+print '\t// Set TDR Style'
+print '\tsetTDRStyle();'
 print ''
-print '   gr->Draw("ALP");'
+print '//Define the variables'
+print '\tFloat_t %s, %s, %s;'%(VarInTextFile[0],VarInTextFile[1],VarInTextFile[2])
+print '\tvector<float> v_%s, v_%s, v_%s;'%(VarInTextFile[0],VarInTextFile[1],VarInTextFile[2])
+print ''
+print ''
+print '\tInt_t nlines = 0;'
+print '\tTFile *f = new TFile("%s.root","RECREATE");'%OutPutCodeName
+print '\tTNtuple *ntuple = new TNtuple("ntuple","data from ascii file","%s:%s:%s");'%(VarInTextFile[0],VarInTextFile[1],VarInTextFile[2])
+print ''
+print ''
+print '\tTCanvas* c1 = new TCanvas("c1","",1);'
+print '\tc1->Range(0,0,1,1);'
+print '\tTPad *pad = new TPad("pad","",0,0,1,1);'
+print '\tpad->SetGrid();'
+print '\tpad->Draw();'
+print '\tpad->cd();'
+print ''
+print ''
+
+print '\tstring line;'
+print '\twhile(getline(in,line))'
+print '\t{'
+print "\t\tif(line[0] == '#') continue;"
+print '\t\t'
+print '\t\tstringstream(line) >> %s >> %s >> %s;'%(VarInTextFile[0],VarInTextFile[1],VarInTextFile[2])
+print '\t\t'
+print '\t\tcout<<"===> "<<%s <<"\t "<<%s<<"\t"<<%s<<endl;'%(VarInTextFile[0],VarInTextFile[1],VarInTextFile[2])
+print '\t\tv_%s.push_back(%s);'%(VarInTextFile[0],VarInTextFile[0])
+print '\t\tv_%s.push_back(%s);'%(VarInTextFile[1],VarInTextFile[1])
+print '\t\tv_%s.push_back(%s);'%(VarInTextFile[2],VarInTextFile[2])
+print '\t\tntuple->Fill(%s,%s,%s);'%(VarInTextFile[0],VarInTextFile[1],VarInTextFile[2])
+print '\t}'
+
+print '\tin.close();'
+print '\t'
+print '\tTGraphErrors * gr = new TGraphErrors(v_%s.size()); '%VarInTextFile[0]
+print '\t    '
+print '\tfor (unsigned int i = 0; i<v_%s.size();i++)'%VarInTextFile[0]
+print '\t{'
+print '\t        gr->SetPoint(i,v_%s[i],v_%s[i]);'%(VarInTextFile[0],VarInTextFile[1])
+print '\t        gr->SetPointError(i,0,v_%s[i]);'%VarInTextFile[2]
+print '\t}'
+ 
+print '\t'
+print '\t'
+
+
+print '\tgr->SetTitle("");'
+print '\t//gr->SetTitle("%s vs %s");'%(VarInTextFile[0],VarInTextFile[1])
+print '\tgr->GetXaxis()->SetTitle("%s");'%xlabel
+print '\tgr->GetYaxis()->SetTitle("%s");'%ylabel
+print '\tgr->GetYaxis()->SetTitleOffset(%i);'%yoffset
+print '\tgr->GetXaxis()->SetTitleOffset(%i);'%xoffset
+print '\tgr->GetYaxis()->SetTitleSize(0.05);'
+print '\tgr->GetXaxis()->SetTitleSize(0.05);'
+print '\t//gr->GetXaxis()->SetLabelSize(0.05);'
+print '\tgr->GetYaxis()->SetRangeUser(%i,%i);'%(yrange[0],yrange[1])
+print '\tgr->SetMarkerSize(1);'
+print '\tgr->SetMarkerColor(2);'
+print '\tgr->SetMarkerStyle(21);'
+print '\t'
+print '\tgr->Draw("ALP");'
+print '\t'
+for leg in range(0,len(legends)):
+	print '\tTLatex *text%i = new TLatex(%i,%i,"%s");'%(leg,pos1,pos2-((pos2/100.)*sep*leg),legends[leg])
+
 print ''
 for leg in range(0,len(legends)):
-	print 'TLatex *text%i = new TLatex(%i,%i,"%s");'%(leg,pos1,pos2-((pos2/100.)*sep*leg),legends[leg])
-
+	print '\ttext%i->SetTextFont(42);'%leg
 print ''
 for leg in range(0,len(legends)):
-	print 'text%i->SetTextFont(42);'%leg
+	print '\ttext%i->SetTextSize(0.05);'%leg
 print ''
 for leg in range(0,len(legends)):
-	print 'text%i->SetTextSize(0.05);'%leg
-print ''
-for leg in range(0,len(legends)):
-	print 'text%i->Draw("same");'%leg
+	print '\ttext%i->Draw("same");'%leg
 print ''
 print ''
 print ''
-print '   TLatex *cmsprem = new TLatex(%i,%i,"#it{CMS Preliminary}");'%(cpos1,cpos2)
+print '\tTLatex *cmsprem = new TLatex(%i,%i,"#it{CMS Preliminary}");'%(cpos1,cpos2)
 print ''
-print '   TLatex *gen = new TLatex(%i,%i,"GE1/1");'%(cpos3,cpos2)
+print '\tTLatex *gen = new TLatex(%i,%i,"%s");'%(cpos3,cpos2,DetInfo)
 print ''
-print '   cmsprem->Draw("same");'
-print '   gen->Draw("same");'
+print '\tcmsprem->Draw("same");'
+print '\tgen->Draw("same");'
 print ''
 print ''
 print ''
-print '   c1->Write();'
-print '   c1->SaveAs("HighVoltage_Vs_SpaceResolution.pdf");'
-print '   c1->SaveAs("HighVoltage_Vs_SpaceResolution.png");'
-print '   c1->SaveAs("HighVoltage_Vs_SpaceResolution.root");'
-print '   c1->SaveAs("HighVoltage_Vs_SpaceResolution.C");'
-print '   f->Write();'
+print '\tc1->Write();'
+print '\tc1->SaveAs("%s.pdf");'%OutPutCodeName
+print '\tc1->SaveAs("%s.png");'%OutPutCodeName
+print '\tc1->SaveAs("%s.root");'%OutPutCodeName
+print '\tf->Write();'
 
 print '}'
 
